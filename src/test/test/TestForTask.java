372019ca6eaeed3b900d28f2ddd477c3969cb534
Nikolay <koloskovnick@yandex.ru>
2024-06-25 01:16:19 +0300

    SomeTest2

--- src/test/test/TestForTask.java
+++ src/test/test/TestForTask.java
@@ -15,7 +15,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 
-public class TaskTest {
+public class TestForTask {
 
     Managers managers = new Managers();
     InMemoryTaskManager manager = (InMemoryTaskManager) managers.getDefault();
